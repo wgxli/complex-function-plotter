86eef7acca2ad9c38d7d3de67408c7faa9d462c4
Samuel Li <contact@samuelj.li>
2021-01-28 02:36:51 -0500

    Function examples disable custom function (if present)

--- src/App.js
+++ src/App.js
@@ -33,240 +33,246 @@ const defaultShader = `vec2 mapping(vec2 z) {
 }`;
 
 class App extends React.Component {
-  state = {
-    expressionText: 'z',
-    expression: ['variable', 'z'],
-    expressionError: false,
+    state = {
+        expressionText: 'z',
+        expression: ['variable', 'z'],
+        expressionError: false,
 
-    customShader: defaultShader,
-    shaderError: false,
+        customShader: defaultShader,
+        shaderError: false,
 
-    menuOpen: false,
-    helpOpen: false,
+        menuOpen: false,
+        helpOpen: false,
 
-    integrationStrategy: null,
-
-    variables: {
-      log_scale: 5,
-      center_x: 0,
-      center_y: 0,
-      t: 0.3,
-      enable_checkerboard: 1,
-      invert_gradient: 0,
-      continuous_gradient: 0,
-      custom_function: 0,
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', this.handleHashChange.bind(this));
-
-    document.addEventListener(
-      'gesturestart',
-      (event) => this.preventDefault(event),
-      {passive: false}
-    );
-    document.addEventListener(
-      'touchmove',
-      (event) => this.preventDefault(event),
-      {passive: false}
-    );
-
-    this.handleHashChange();
-  }
-
-  preventDefault(event) {
-    const tagName = event.target.tagName.toLowerCase();
-    if (tagName === 'canvas') {
-      event.preventDefault();
-    } else {
-      return event;
-    }
-  }
-
-  // Toolbar buttons
-  handleMenuButton() {
-    this.setState({menuOpen: !this.state.menuOpen});
-  }
-
-  handleHelpButton() {
-    this.setState({helpOpen: !this.state.helpOpen});
-  }
-
-  // Variable Sliders
-  handleVariableUpdate(variables) {
-    this.state.variables = variables;
-    this.refs.plot.handleVariableUpdate(variables);
-  }
-
-  handleVariableAdd(name, value) {
-    const variables = this.state.variables;
-    variables[name] = value;
-    this.setState({variables: variables});
-  }
-
-  handleVariableRemove(name) {
-    const variables = this.state.variables;
-    delete variables[name];
-    this.setState({variables: variables});
-  }
-
-  handleOptionToggle(name) {
-    const variables = this.state.variables;
-    variables[name] = (variables[name] < 0.5) ? 1 : 0;
-    this.setState({variables: variables});
-  }
-
-  setErrorMessage(message) {
-    const {variables, expressionError, shaderError} = this.state;
-
-    if (variables.custom_function > 0.5) {
-      if (message !== shaderError) {
-	this.setState({shaderError: message});
-      }
-    } else {
-      if (message !== expressionError) {
-	this.setState({expressionError: message});
-      }
-    }
-  }
-
-  handleHashChange() {
-    const hash = window.location.hash;
-    if (hash === '') {
-      return;
-    } else {
-      this.setExpression(decodeURIComponent(hash.slice(1)));
-    }
-  }
-
-  setExpression(text) {
-    text = text.toLowerCase();
-
-    window.history.replaceState(
-      undefined, undefined,
-      '#' + encodeURIComponent(text)
-    );
-
-    this.setState({
-        expressionText: text,
-        expression: parseExpression(text.trim()),
         integrationStrategy: null,
-    });
-  }
 
-  setCustomShader(text) {
-    this.setState({customShader: text});
-  }
-
-  renderFunctionEditor() {
-    const {variables, customShader, shaderError} = this.state;
-    if (variables.custom_function > 0.5) {
-      return (
-	<FunctionEditor
-	  onChange={this.setCustomShader.bind(this)}
-	  errorMessage={shaderError}
-	  value={customShader}
-	/>
-      );
-    } else {
-      return; 
+        variables: {
+            log_scale: 5,
+            center_x: 0,
+            center_y: 0,
+            t: 0.3,
+            enable_checkerboard: 1,
+            invert_gradient: 0,
+            continuous_gradient: 0,
+            custom_function: 0,
+        }
     }
-  }
 
-  render() {
-    const {
-        variables,
-        customShader,
-        expression, expressionText,
-        menuOpen, helpOpen,
-        expressionError,
-        integrationStrategy,
-    } = this.state;
+    componentDidMount() {
+        window.addEventListener('hashchange', this.handleHashChange.bind(this));
 
-    const useCustomShader = variables.custom_function > 0.5;
+        document.addEventListener(
+            'gesturestart',
+            (event) => this.preventDefault(event),
+            {passive: false}
+        );
+        document.addEventListener(
+            'touchmove',
+            (event) => this.preventDefault(event),
+            {passive: false}
+        );
 
-    const ast = useCustomShader ? customShader : expression;
+        this.handleHashChange();
+    }
 
-    return (
-      <MuiThemeProvider theme={theme}>
-	<div id='app'>
-	  <ControlBar
-	    menuOpen={menuOpen}
-	    helpOpen={helpOpen}
-	    onMenuButton={this.handleMenuButton.bind(this)}
-	    onHelpButton={this.handleHelpButton.bind(this)}
+    preventDefault(event) {
+        const tagName = event.target.tagName.toLowerCase();
+        if (tagName === 'canvas') {
+            event.preventDefault();
+        } else {
+            return event;
+        }
+    }
 
-	    onChange={this.setExpression.bind(this)}
-	    value={expressionText}
-	    error={expressionError}
-	  />
-	  <SidePanel
-	    className='control-panel'
-	    open={menuOpen} 
-	    onToggle={this.handleMenuButton.bind(this)}
-	    anchor='left'
-	    transitionWidth={480}
-	  >
-	    <SliderPanel
-	      variables={variables}
-	      onUpdate={this.handleVariableUpdate.bind(this)}
-	      onAdd={this.handleVariableAdd.bind(this)}
-	      onRemove={this.handleVariableRemove.bind(this)}
-	    />
-            <IntegralPanel
-              variables={variables}
-              hidePanels={() => this.setState({helpOpen: false, menuOpen: false})}
-              openCalculator={integrationStrategy => this.setState({integrationStrategy})}
-            />
-	    <OptionsPanel
-	      heading='Graphics Options'
-	      options={{
-		enable_checkerboard: 'Enable Checkerboard',
-		invert_gradient: 'Invert Gradient',
-		continuous_gradient: 'Continuous Gradient',
-	      }}
-	      onToggle={this.handleOptionToggle.bind(this)}
-	      variables={variables}
-	    />
-	    <OptionsPanel
-	      heading='Advanced Options'
-	      options={{
-		custom_function: 'Custom Function'
-	      }}
-	      onToggle={this.handleOptionToggle.bind(this)}
-	      variables={variables}
-	    />
-	    {this.renderFunctionEditor()}
-	  </SidePanel>
-          <IntegralCalculator
-              expression={ast}
-              variables={variables}
-              integrator={integrationStrategy}
-              onClose={() => this.setState({integrationStrategy: null})}
-          />
-	  <FunctionPlot
-	    ref='plot'
-	    expression={ast}
-	    variables={variables}
-	    onError={this.setErrorMessage.bind(this)}
-	  />
-	  <SidePanel
-	    className='help-panel'
-	    open={helpOpen}
-	    anchor='right'
-	    onToggle={this.handleHelpButton.bind(this)}
-	    transitionWidth={900}
-	  >
-	    <HelpText
-              setExpression={this.setExpression.bind(this)}
-              closeMenu={() => this.setState({helpOpen: false})}
-            />
-	  </SidePanel>
-	</div>
-      </MuiThemeProvider>
-    );
-  }
+    // Toolbar buttons
+    handleMenuButton() {
+        this.setState({menuOpen: !this.state.menuOpen});
+    }
+
+    handleHelpButton() {
+        this.setState({helpOpen: !this.state.helpOpen});
+    }
+
+    // Variable Sliders
+    handleVariableUpdate(variables) {
+        this.state.variables = variables;
+        this.refs.plot.handleVariableUpdate(variables);
+    }
+
+    handleVariableAdd(name, value) {
+        const variables = this.state.variables;
+        variables[name] = value;
+        this.setState({variables: variables});
+    }
+
+    handleVariableRemove(name) {
+        const variables = this.state.variables;
+        delete variables[name];
+        this.setState({variables: variables});
+    }
+
+    handleOptionToggle(name) {
+        const variables = this.state.variables;
+        variables[name] = (variables[name] < 0.5) ? 1 : 0;
+        this.setState({variables: variables});
+    }
+
+    setErrorMessage(message) {
+        const {variables, expressionError, shaderError} = this.state;
+
+        if (variables.custom_function > 0.5) {
+            if (message !== shaderError) {
+                this.setState({shaderError: message});
+            }
+        } else {
+            if (message !== expressionError) {
+                this.setState({expressionError: message});
+            }
+        }
+    }
+
+    handleHashChange() {
+        const hash = window.location.hash;
+        if (hash === '') {
+            return;
+        } else {
+            this.setExpression(decodeURIComponent(hash.slice(1)));
+        }
+    }
+
+    setExpression(text) {
+        text = text.toLowerCase();
+
+        window.history.replaceState(
+            undefined, undefined,
+            '#' + encodeURIComponent(text)
+        );
+
+        this.setState({
+            expressionText: text,
+            expression: parseExpression(text.trim()),
+            integrationStrategy: null,
+        });
+    }
+
+    setCustomShader(text) {
+        this.setState({customShader: text});
+    }
+
+    renderFunctionEditor() {
+        const {variables, customShader, shaderError} = this.state;
+        if (variables.custom_function > 0.5) {
+            return (
+                <FunctionEditor
+                    onChange={this.setCustomShader.bind(this)}
+                    errorMessage={shaderError}
+                    value={customShader}
+                />
+            );
+        } else {
+            return; 
+        }
+    }
+
+    render() {
+        const {
+            variables,
+            customShader,
+            expression, expressionText,
+            menuOpen, helpOpen,
+            expressionError,
+            integrationStrategy,
+        } = this.state;
+
+        const useCustomShader = variables.custom_function > 0.5;
+
+        const ast = useCustomShader ? customShader : expression;
+
+        return (
+            <MuiThemeProvider theme={theme}>
+                <div id='app'>
+                    <ControlBar
+                        menuOpen={menuOpen}
+                        helpOpen={helpOpen}
+                        onMenuButton={this.handleMenuButton.bind(this)}
+                        onHelpButton={this.handleHelpButton.bind(this)}
+
+                        onChange={this.setExpression.bind(this)}
+                        value={expressionText}
+                        error={expressionError}
+                    />
+                    <SidePanel
+                        className='control-panel'
+                        open={menuOpen} 
+                        onToggle={this.handleMenuButton.bind(this)}
+                        anchor='left'
+                        transitionWidth={480}
+                    >
+                        <SliderPanel
+                            variables={variables}
+                            onUpdate={this.handleVariableUpdate.bind(this)}
+                            onAdd={this.handleVariableAdd.bind(this)}
+                            onRemove={this.handleVariableRemove.bind(this)}
+                        />
+                        <IntegralPanel
+                            variables={variables}
+                            hidePanels={() => this.setState({helpOpen: false, menuOpen: false})}
+                            openCalculator={integrationStrategy => this.setState({integrationStrategy})}
+                        />
+                        <OptionsPanel
+                            heading='Graphics Options'
+                            options={{
+                                enable_checkerboard: 'Enable Checkerboard',
+                                invert_gradient: 'Invert Gradient',
+                                continuous_gradient: 'Continuous Gradient',
+                            }}
+                            onToggle={this.handleOptionToggle.bind(this)}
+                            variables={variables}
+                        />
+                        <OptionsPanel
+                            heading='Advanced Options'
+                            options={{
+                                custom_function: 'Custom Function'
+                            }}
+                            onToggle={this.handleOptionToggle.bind(this)}
+                            variables={variables}
+                        />
+                        {this.renderFunctionEditor()}
+                    </SidePanel>
+                    <IntegralCalculator
+                        expression={ast}
+                        variables={variables}
+                        integrator={integrationStrategy}
+                        onClose={() => this.setState({integrationStrategy: null})}
+                    />
+                    <FunctionPlot
+                        ref='plot'
+                        expression={ast}
+                        variables={variables}
+                        onError={this.setErrorMessage.bind(this)}
+                    />
+                    <SidePanel
+                        className='help-panel'
+                        open={helpOpen}
+                        anchor='right'
+                        onToggle={this.handleHelpButton.bind(this)}
+                        transitionWidth={900}
+                    >
+                        <HelpText
+                            setExpression={(expression) => {
+                                this.setExpression(expression);
+                                this.handleVariableUpdate({
+                                    ...this.state.variables,
+                                    custom_function: 0,
+                                });
+                            }}
+                            closeMenu={() => this.setState({helpOpen: false})}
+                        />
+                    </SidePanel>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
