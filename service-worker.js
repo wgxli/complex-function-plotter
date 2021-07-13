26bec413ea7b19818ddb3ac95aa2407e1f1cc80c
Samuel Li <contact@samuelj.li>
2021-07-12 23:49:44 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/complex-function-plotter/precache-manifest.ebbd11ee2fe9720c3541c8602a5117e1.js"
+  "/complex-function-plotter/precache-manifest.bd1d2cb4c233892fba4c59e3487ac046.js"
 );
 
 workbox.clientsClaim();
