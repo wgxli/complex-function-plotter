Fix Riemann-Siegel formula

src/gl-code/scratch/eta.py
@@ -1,20 +1,18 @@
 """Borwein series for Dirichlet eta."""
 import math
+import numpy as np
 from scipy.special import factorial as fact
 
-n = 16
+n = 49
 
 d = [n * sum(fact(n+l-1) * 4**l / (fact(n-l) * fact(2*l)) for l in range(k+1)) for k in range(n+1)]
-print(d)
 
 coefficients = [((-1)**k * (d[n] - d[k]))/d[n] for k in range(n)]
-print(coefficients)
 
-print('positives:')
-for k in range(0, n, 2):
-    print(f'{-math.log(k+1):.12f} * exp_z + vec2({math.log(coefficients[k]):.12f}, 0.),')
-print()
+coefficients = np.array(coefficients)[1:]
+ns = np.arange(n+1)[2:]
 
-print('negatives:')
-for k in range(1, n, 2):
-    print(f'{-math.log(k+1):.12f} * exp_z + vec2({math.log(-coefficients[k]):.12f}, 0.),')
+for i in [0, 16, 32]:
+    print('mat4(' + ','.join(f'-{x:.14f}' for x in np.log(ns[i:i+16])) + '),')
+    print('mat4(' + ','.join(f'{x:.14f}' for x in coefficients[i:i+16]) + '));')
+    print()

src/gl-code/scratch/zeta.py
@@ -1,11 +1,7 @@
 """Borwein series for Dirichlet eta."""
 import math
+import numpy as np
 
-N = 8
-
-for n in range(1, N+1):
-    print(f'{-math.log(n):.12f} * exp_z,')
-print()
-
-for n in range(1, N+1):
-    print(f'{math.log(n):.12f} * exp_z - vec2({math.log(n):.12f}, 0),')
+ns = 2 + 16*1 + np.arange(16)
+print('mat4(' + ','.join(f'{x:.0f}.' for x in ns) + '),')
+print('mat4(' + ','.join(f'-{x:.12f}' for x in np.log(ns)) + ')')